f5c1a5763cd80230e479503f4ed8db159ab6e0c0
Alexander Wazzan <alexwazzan@g.ucla.edu>
2023-04-17 21:35:39 +0000

    Added support for reading HTTP requests
    Changes:
    - Instead of writing immediately after each read, only write once the entire request is received
    - Added a status line to the response
    
    Change-Id: I760a477c2b3b3393d9771f6614ca14d4142f3d67

--- include/session.h
+++ include/session.h
@@ -20,13 +20,10 @@ class session {
             size_t bytes_transferred);
         void handle_write(const boost::system::error_code& error);
 
-        void handle_request_read(const boost::system::error_code& error,
-            size_t bytes_transferred);
-        void handle_request_write(const boost::system::error_code& error);
-
         tcp::socket socket_;
 
-        // Max length constant chosen based to support 64K HTTP request size
+        int bytes_read_;
+        // Max length constant chosen to support 64K HTTP request size
         // Per: https://piazza.com/class/lfx37nmbiup24h/post/118
         enum { max_length = 64000 };
         char data_[max_length];
--- src/session.cc
+++ src/session.cc
@@ -1,24 +1,26 @@
-#include "session.h"
-
 #include <cstdlib>
 #include <iostream>
 
 #include <boost/asio.hpp>
 #include <boost/bind.hpp>
 
+#include "session.h"
+
 using boost::asio::ip::tcp;
 
 session::session(boost::asio::io_service& io_service)
     : socket_(io_service)
-{}
+{
+    bytes_read_ = 0;
+}
 
 tcp::socket& session::socket() {
     return socket_;
 }
 
 void session::start() {
-    socket_.async_read_some(boost::asio::buffer(data_, max_length),
-        boost::bind(&session::handle_request_read, this,
+    socket_.async_read_some(boost::asio::buffer(data_ + bytes_read_, max_length - bytes_read_),
+        boost::bind(&session::handle_read, this,
         boost::asio::placeholders::error,
         boost::asio::placeholders::bytes_transferred));
 }
@@ -26,10 +28,36 @@ void session::start() {
 void session::handle_read(const boost::system::error_code& error,
       size_t bytes_transferred) {
     if (!error) {
-        boost::asio::async_write(socket_,
-            boost::asio::buffer(data_, bytes_transferred),
-            boost::bind(&session::handle_write, this,
-            boost::asio::placeholders::error));
+        bytes_read_ += bytes_transferred;
+        
+        const std::string header_delimiter = "\r\n\r\n";
+        const std::string response_code = "HTTP/1.1 200 0K\r\n";
+        const std::string content_type = "Content-Type: text/plain" + header_delimiter;
+
+        // Check whether 2x CRLF exists:
+        char * header_end = strstr(data_, header_delimiter.c_str());
+
+        // If it does exist, we have received the entire header:
+        if (header_end) {
+            int header_size = header_end - data_;
+
+            // Delimit the end of the header with NULL:
+            data_[header_size] = 0;
+            std::string content = data_;
+
+            // Construct and send the response:
+            response_ = response_code + content_type + content + "\r\n";
+            boost::asio::async_write(socket_, 
+                boost::asio::buffer(response_, response_.size()),
+                boost::bind(&session::handle_write, this,
+                boost::asio::placeholders::error));
+        // If we have not reached the end of the header, continue reading:
+        } else {
+            socket_.async_read_some(boost::asio::buffer(data_ + bytes_read_, max_length - bytes_read_),
+                boost::bind(&session::handle_read, this,
+                boost::asio::placeholders::error,
+                boost::asio::placeholders::bytes_transferred));
+        }
     }
     else {
         delete this;
@@ -38,41 +66,12 @@ void session::handle_read(const boost::system::error_code& error,
 
 void session::handle_write(const boost::system::error_code& error) {
     if (!error) {
-        socket_.async_read_some(boost::asio::buffer(data_, max_length),
+        // Upon writing, reset all instance variables:
+        memset(data_, 0, bytes_read_);
+        bytes_read_ = 0;
+
+        socket_.async_read_some(boost::asio::buffer(data_ + bytes_read_, max_length - bytes_read_),
             boost::bind(&session::handle_read, this,
-            boost::asio::placeholders::error,
-            boost::asio::placeholders::bytes_transferred));
-    }
-    else {
-        delete this;
-    }
-}
-
-void session::handle_request_read(const boost::system::error_code& error,
-      size_t bytes_transferred) {
-    if (!error) {
-        const std::string delimiter = "\r\n\r\n";
-
-        std::string response_code = "HTTP/1.1 200 0K\r\n";
-        std::string content_type = "text/plain\r\n\n";
-        std::string content = data_;
-
-        response_ = response_code + content_type + content + "\r\n";
-
-        boost::asio::async_write(socket_, 
-            boost::asio::buffer(response_, response_.size()),
-            boost::bind(&session::handle_request_write, this,
-            boost::asio::placeholders::error));
-    }
-    else {
-        delete this;
-    }
-}
-
-void session::handle_request_write(const boost::system::error_code& error) {
-    if (!error) {
-        socket_.async_read_some(boost::asio::buffer(data_, max_length),
-            boost::bind(&session::handle_request_read, this,
             boost::asio::placeholders::error,
             boost::asio::placeholders::bytes_transferred));
     }
