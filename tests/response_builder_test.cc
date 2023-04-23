6ce9a1d06da781aa681d38fe1453d0abd78ae004
Alexander Wazzan <alexwazzan@g.ucla.edu>
2023-04-23 04:54:15 +0000

    Added tests for ResponseBuilder
    Change-Id: I57fadaca4e49045edbb1cada26a63feab84d5518

---
+++ tests/response_builder_test.cc
@@ -0,0 +1,41 @@
+#include "response_builder.h"
+#include "gtest/gtest.h"
+
+class ResponseBuilderTest : public ::testing::Test {
+  protected:
+    void SetUp() override { }
+
+    ResponseBuilder response_builder_;
+    const std::string delimiter_ = "\r\n\r\n";
+    const std::string response_code_ = "HTTP/1.1 200 0K\r\n";
+    const std::string content_type_ = "Content-Type: text/plain" + delimiter_;
+};
+
+TEST_F(ResponseBuilderTest, SimpleRequestAndReset) {
+    char data[] = "This is a sample request.\r\n\r\n";
+
+    int return_value = response_builder_.formatResponse(data,
+        delimiter_,
+        response_code_,
+        content_type_);
+    
+    EXPECT_EQ(return_value, 1);
+    EXPECT_EQ(response_builder_.getResponse(),
+        "HTTP/1.1 200 0K\r\nContent-Type: text/plain\r\n\r\nThis is a sample request.\r\n");
+    
+    response_builder_.resetResponse();
+
+    EXPECT_EQ(response_builder_.getResponse(), "");
+}
+
+TEST_F(ResponseBuilderTest, NoDelimiterRequest) {
+    char data[] = "This is a sample request.";
+
+    int return_value = response_builder_.formatResponse(data,
+        delimiter_,
+        response_code_,
+        content_type_);
+    
+    EXPECT_EQ(return_value, 0);
+    EXPECT_EQ(response_builder_.getResponse(), "");
+}
