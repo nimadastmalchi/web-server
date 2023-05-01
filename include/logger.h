f257d0eeded4b18ed341d3803c576abc5ea41037
Devel User <michaelyu678@g.ucla.edu>
2023-05-01 00:05:30 +0000

    Add logger functions and tests
    - This change is needed to consolidate the logging functions to the primary Logger object
    
    - This ensures that any universal changes to logging can be done through the single Logger object rather than changing every use of logging throughout the code
    
    Change-Id: Iac5345b9f5a93f9a38995b28b5d88e4822620b93

--- include/logger.h
+++ include/logger.h
@@ -5,9 +5,26 @@
 
 class Logger {
     public: 
+
+        struct RotationTime {
+            int hour;
+            int minute;
+            int second;
+
+            RotationTime(int hour, int minute, int second): hour(hour), minute(minute), second(second) { }
+        };
+
         Logger();
+        void init(std::string file_name, int rotation_size, RotationTime rotation_time);
+        void log_trace(std::string msg);
+        void log_debug(std::string msg);
+        void log_info(std::string msg);
+        void log_warn(std::string msg);
+        void log_error(std::string msg);
         static std::shared_ptr<Logger> logger();
+  
     private:
+  
         static std::shared_ptr<Logger> logger_;
 };
 
--- tests/logger_test.cc
+++ tests/logger_test.cc
@@ -1,11 +1,96 @@
 #include "logger.h"
 #include "gtest/gtest.h"
 
+#include <boost/regex.hpp>
+#include <boost/thread/thread.hpp>
+#include <filesystem>
+#include <regex>
+
 class LoggerTest : public ::testing::Test {
   protected:
-    void SetUp() override { }
+    void SetUp() override { 
+      	// 10 * 1024 * 1024 = 10MB rotation size
+      	// 0, 0, 0 = 12:00:00 rotation time
+        Logger::logger()->init("test.log", 10 * 1024 * 1024, {
+            0, 0, 0
+        });
+        testing::internal::CaptureStdout();
+    }
+
+    void TearDown() {
+        std::filesystem::remove("test.log");
+    }
 };
+
+struct LogSplit {
+    std::string timestamp;
+    std::string thread_id;
+    std::string sev;
+    std::string msg;
+};
+
+LogSplit split_log(std::string log) {
+    boost::regex log_regex {"\\[(.+)\\] \\(Thread: (.+)\\) \\[(.*)\\]: (.+)\n"};
+    boost::smatch match;
+    boost::regex_match(log, match, log_regex);
+    return LogSplit {
+       match[1],
+       match[2],
+       match[3],
+       match[4]
+    };
+}
 
 TEST_F(LoggerTest, SingletonLoggerCreated) {
     EXPECT_EQ(Logger::logger(), Logger::logger());
+}
+
+TEST_F(LoggerTest, LogTrace) {
+    std::string msg = "Test";
+    Logger::logger()->log_trace(msg);
+    std::string output = testing::internal::GetCapturedStdout();
+    std::cout << output << std::endl;
+    auto log_components = split_log(output);
+    EXPECT_EQ(log_components.msg, msg);
+    EXPECT_EQ(log_components.sev, "trace");
+}
+
+TEST_F(LoggerTest, LogDebug) {
+    std::string msg = "Test";
+    Logger::logger()->log_debug(msg);
+    std::string output = testing::internal::GetCapturedStdout();
+    std::cout << output << std::endl;
+    auto log_components = split_log(output);
+    EXPECT_EQ(log_components.msg, msg);
+    EXPECT_EQ(log_components.sev, "debug");
+}
+
+TEST_F(LoggerTest, LogInfo) {
+    std::string msg = "Test";
+    Logger::logger()->log_info(msg);
+    std::string output = testing::internal::GetCapturedStdout();
+    std::cout << output << std::endl;
+    auto log_components = split_log(output);
+    EXPECT_EQ(log_components.msg, msg);
+    EXPECT_EQ(log_components.sev, "info");
+}
+
+TEST_F(LoggerTest, LogWarn) {
+    std::string msg = "Test";
+    Logger::logger()->log_warn(msg);
+    std::string output = testing::internal::GetCapturedStdout();
+    std::cout << output << std::endl;
+    auto log_components = split_log(output);
+    EXPECT_EQ(log_components.msg, msg);
+    EXPECT_EQ(log_components.sev, "warning");
+}
+
+TEST_F(LoggerTest, LogError) {
+    std::string msg = "Test";
+    Logger::logger()->log_error(msg);
+    std::string output = testing::internal::GetCapturedStdout();
+    std::cout << output << std::endl;
+    auto log_components = split_log(output);
+    EXPECT_EQ(log_components.msg, msg);
+    EXPECT_EQ(log_components.sev, "error");
 }
